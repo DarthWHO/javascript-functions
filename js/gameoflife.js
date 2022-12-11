1c7d18538f17f96b3ceb3217c2e29afbebbec78f
Scott Tibbo <stibbo@gmail.com>
2022-12-10 22:55:21 -0700

    post printcell

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -12,7 +12,14 @@ function contains(cell) {
   return this.some((thisCell) => same(thisCell, cell));
 }
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  if (contains.call(state, cell)) {
+    return '\u25A3'
+  }
+  else {
+    return '\u25A2'
+  }
+};
 
 const corners = (state = []) => {};
 
